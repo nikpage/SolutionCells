df03f51bacd2107ad49b45e7888242b50b04d950
Nik Page <nbpage@gmail.com>
2025-02-16 16:39:27 +0100

    Update translations.py

--- utils/translations.py
+++ utils/translations.py
@@ -4,5 +4,5 @@ from database import get_user_language
 def get_text(key: str, user_id: int, **kwargs) -> str:
     """Get translated text for given key and user."""
     lang = get_user_language(user_id)
-    text = TRANSLATIONS[lang].get(key, TRANSLATIONS['en'][key])
+    text = TRANSLATIONS[lang].get(key, TRANSLATIONS['en'].get(key, key))
     return text.format(**kwargs) if kwargs else text
